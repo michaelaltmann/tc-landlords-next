e90bf80b782940c430f533423022e9f4f4f0b145
maltmann <michael.altmann@gmail.com>
2023-04-01 13:27:20 -0500

    More parcel props and tabs for portfolio

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -8,8 +8,9 @@ export default function Portfolio({ parcel }: ParcelPrams) {
   const { data: parcels } = useParcel().findMany({
     where: { portfolio_id: parcel?.portfolio_id },
   });
+  const tabs = ["List", "Map"];
 
-  const [mode, setMode] = useState("LIST");
+  const [mode, setMode] = useState(tabs[0]);
 
   function ListView() {
     return (
@@ -24,6 +25,57 @@ export default function Portfolio({ parcel }: ParcelPrams) {
       </>
     );
   }
-  if (mode === "LIST") return <ListView />;
-  else return <>Unknown view mode</>;
+
+  function buildTabHeaders() {
+    return (
+      <div className="border-b border-gray-200 text-center text-sm font-medium text-gray-500 dark:border-gray-700 dark:text-gray-400">
+        <ul className="-mb-px flex flex-wrap">
+          {tabs.map((tab) => {
+            if (tab.toUpperCase() === mode?.toUpperCase())
+              return (
+                <li
+                  className="mr-2"
+                  onClick={() => {
+                    setMode(tab);
+                  }}
+                >
+                  <a
+                    href="#"
+                    aria-current="page"
+                    className="active inline-block rounded-t-lg bg-gray-100 p-4 text-blue-600 dark:bg-gray-800 dark:text-blue-500"
+                  >
+                    {tab}
+                  </a>
+                </li>
+              );
+            else
+              return (
+                <li
+                  className="mr-2"
+                  onClick={() => {
+                    setMode(tab);
+                  }}
+                >
+                  <a
+                    href="#"
+                    className="inline-block rounded-t-lg p-4 hover:bg-gray-50 hover:text-gray-600 dark:hover:bg-gray-800 dark:hover:text-gray-300"
+                  >
+                    {tab}
+                  </a>
+                </li>
+              );
+          })}
+        </ul>
+      </div>
+    );
+  }
+  function buildTabs() {
+    if (mode?.toUpperCase() === "LIST") return <ListView />;
+    else return <>Unknown view mode {mode}</>;
+  }
+  return (
+    <>
+      {buildTabHeaders()} {buildTabs()}
+    </>
+  );
 }
--- src/pages/parcel/[id].tsx
+++ src/pages/parcel/[id].tsx
@@ -1,7 +1,7 @@
 import { useParcel, useKeyword } from "../../lib/hooks";
 import { useRouter } from "next/router";
 import Portfolio from "../../components/Portfolio";
-
+import { BiLinkExternal } from "react-icons/bi";
 export default function ParcelView() {
   const router = useRouter();
   const { id } = router.query;
@@ -13,12 +13,83 @@ export default function ParcelView() {
     const { data: keywords } = useKeyword().findMany({
       where: { parcel_id: id?.toString().toUpperCase() },
     });
-
+    const upper_county = parcel?.county?.toUpperCase();
+    let parcel_url = undefined;
+    const county_pid = parcel?.id?.split("-")[3] || "";
+    switch (upper_county) {
+      case "HENNEPIN":
+        parcel_url = `https://www16.co.hennepin.mn.us/pins/pidresult.jsp?pid=${county_pid}`;
+        break;
+      case "RAMSEY":
+        parcel_url = `https://beacon.schneidercorp.com/application.aspx?AppID=959&LayerID=18852&PageTypeID=4&PageID=8397&Q=1822844214&KeyValue=${county_pid}`;
+        break;
+      case "SCOTT":
+        parcel_url = `https://publicaccess.scottcountymn.gov/Datalets/Datalet.aspx?pin=${county_pid}`;
+        break;
+      case "WASHINGTON":
+        parcel_url = `https://mn-washington.manatron.com/Tabs/TaxSearch/AccountDetail.aspx?p=${county_pid}`;
+        break;
+      case "DAKOTA":
+        parcel_url = `https://gis.co.dakota.mn.us/PropertyCard/PropertyCard.aspx?pin=${county_pid}`;
+        break;
+      case "ANOKA":
+        parcel_url = undefined;
+        break;
+      case "CARVER":
+        if (county_pid) {
+          const pid =
+            county_pid.substring(0, 2) + "." + county_pid.substring(2);
+          parcel_url = `https://mn-carver.manatron.com/Tabs/TaxSearch/ParcelDetail.aspx?a=29124&p=${pid}`;
+        }
+        break;
+      default:
+        parcel_url = undefined;
+    }
     return (
       <div className="ml-4">
         <div>
           <div className="text-center text-xl">Parcel {id}</div>
-          {parcel && <div> {parcel.address}</div>}
+          {parcel && (
+            <>
+              <div>
+                {" "}
+                <b>Address: </b> {parcel.address}
+              </div>
+              <div>
+                <b>Owner: </b>
+                {parcel.owner_name}{" "}
+                {parcel.owner_address && (
+                  <>
+                    <b>at</b> {parcel.owner_address}
+                  </>
+                )}
+              </div>
+              <div>
+                <b>Taxpayer: </b>
+                {parcel.taxpayer_name}
+              </div>
+              <div>
+                <b>Homestead: </b> {parcel.homestead ? "Y" : "N"}{" "}
+              </div>
+              <div>
+                <b>Landlord license: </b> {parcel.license_num}{" "}
+              </div>
+              {parcel_url && (
+                <div>
+                  <b>Tax details: </b>
+                  <a
+                    className="font-medium text-blue-600 hover:underline dark:text-blue-500"
+                    href={parcel_url}
+                    target="_blank"
+                    rel="noreferrer noopener"
+                  >
+                    {parcel.county} county site{" "}
+                    <BiLinkExternal style={{ display: "flow" }} />
+                  </a>
+                </div>
+              )}{" "}
+            </>
+          )}
         </div>
         <div className="text-center text-xl">Keywords</div>
         <ul>
